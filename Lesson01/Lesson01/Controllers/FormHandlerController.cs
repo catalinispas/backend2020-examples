c1b2f52d4e0b8ff2f03e49584a27d6e96490f3d5
jesarbov <jesarbov@gmail.com>
2020-09-01 10:29:39 +0200

    Lesson01_examples

---
+++ Lesson01/Lesson01/Controllers/FormHandlerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson01.Controllers
+{
+    public class FormHandlerController : Controller
+    {
+        // GET: FormHandler
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        // POST: FormHandler
+        [HttpPost]
+        public IActionResult Index(IFormCollection formCollection) {
+            ViewBag.Firstname = formCollection["FirstName"];
+            ViewBag.Lastname = formCollection["LastnNme"];
+            return View();
+        }
+    }
+
+}
+
+
---
+++ Lesson01/Lesson01/Controllers/MovieController.cs
@@ -0,0 +1,33 @@
+﻿using Lesson01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson01.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public IActionResult Index()
+        {
+            ViewData["movie"] = "One Flew Over the Cuckoo's Nest";
+            ViewData["year"] = 1975;
+
+            ViewBag.Movie = "Forrest Gump";
+            ViewBag.Year = 1994;
+
+            return View(new Movie { Title = "Intouchables", Year = 2011 });
+        }
+   
+        // GET: Movie
+        public IActionResult Example01()
+        {
+            ViewBag.Movie = "One Flew Over the Cuckoo's Nest";
+            ViewBag.Year = 1975;
+
+            return View();
+        }
+    }
+}
